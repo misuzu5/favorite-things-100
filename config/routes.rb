7d6a37ee137049c88895f107f285c71eedbad1bd
mino morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-10-26 17:23:24 +0900

    マイページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'favorite_things#index'
   resources :favorite_things, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :users, only: :show
 end
