94f621c0ef64a8660e680d641e32cf4f815630ae
mino morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-10-26 12:23:38 +0900

    編集機能の実装

--- app/controllers/favorite_things_controller.rb
+++ app/controllers/favorite_things_controller.rb
@@ -1,5 +1,8 @@
 class FavoriteThingsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_favorite_thing, except: [:index, :new, :create]
+  before_action :contributor_confirmation, only: [:edit, :update]
+
 
   def index
     @favorite_things = FavoriteThing.includes(:user).order('created_at DESC')
@@ -19,7 +22,17 @@ class FavoriteThingsController < ApplicationController
   end
 
   def show
-    @favorite_thing = FavoriteThing.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @favorite_thing.update(favorite_thing_params)
+      redirect_to favorite_thing_path(@favorite_thing)
+    else
+      render :edit
+    end
   end
 
   private
@@ -27,5 +40,13 @@ class FavoriteThingsController < ApplicationController
   def favorite_thing_params
     params.require(:favorite_thing).permit(:title, :reason, :emotion, :appeal_point, :episode,
                                            :image).merge(user_id: current_user.id)
+  end
+
+  def set_favorite_thing
+    @favorite_thing = FavoriteThing.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @favorite_thing.user
   end
 end
