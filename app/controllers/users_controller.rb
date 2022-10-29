75d56967151c19ed513bdb400766767c7996a5cf
mino morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-10-29 15:12:27 +0900

    リストページ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @favorite_things = @user.favorite_things
+    @titles = @favorite_things.pluck(:title)
   end
 end
