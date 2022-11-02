176cdc720f7bc3ae643cfaf91729b2cd15bbbeb0
mino morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-11-02 11:24:30 +0900

    ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     sessions: "users/sessions",
     registrations: "users/registrations"
   }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+  
   root to: 'favorite_things#index'
   resources :favorite_things, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: :create 
