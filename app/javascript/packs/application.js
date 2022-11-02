0b5ddea5dfdfc954172804dd7e63e73b7851b2b5
mino morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-11-02 16:04:02 +0900

    投稿編集画面 プレビュー画面実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require('./preview')
+require('./preview2')
 require('./preview-sign-up')
 require('./preview-edit')
 import "bootstrap"
