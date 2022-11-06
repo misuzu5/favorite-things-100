dce31159d8f339911bbd1e243c89f4c34619e6c1
mino morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-11-06 18:33:23 +0900

    タイプ名非表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @favorite_things = @user.favorite_things
-    @type_ids = @favorite_things.group(:type_id).count
+    # @type_ids = @favorite_things.group(:type_id).count
     @titles = @favorite_things.pluck(:title)
   end
 end
