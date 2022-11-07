db5e056e2a7c683a032125d510a237a5d0736f8a
mino morimoto <mihomorimoto@Reo-MacBookAir.local>
2022-11-07 10:45:16 +0900

    リスト100 タイトルリンク実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @favorite_things = @user.favorite_things
-    @titles = @favorite_things.pluck(:title)
 
     ary = @favorite_things.pluck(:type_id)
     @type_num = ary.max_by { |v| ary.count(v) }
